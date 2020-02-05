e2c7ecfb15184f38705368e4740dd5b38d7fd072
Liang HUANG <revenol@outlook.com>
2020-02-05 19:04:06 +0800

    PyTorch tensor
    PyTorch version

--- memory.py
+++ memory.py
@@ -2,18 +2,17 @@
 #  This file contains the main DROO operations, including building DNN, 
 #  Storing data sample, Training DNN, and generating quantized binary offloading decisions.
 
-#  version 1.0 -- January 2020. Written based on Tensorflow 2 by Weijian Pan and 
+#  version 1.0 -- February 2020. Written based on Tensorflow 2 by Weijian Pan and 
 #  Liang Huang (lianghuang AT zjut.edu.cn)
-#  #################################################################
+#  ###################################################################
 
 from __future__ import print_function
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import torch
+import torch.optim as optim
+import torch.nn as nn
 import numpy as np
 
-print(tf.__version__)
-print(tf.keras.__version__)
+print(torch.__version__)
 
 
 # DNN network for memory
@@ -22,18 +21,18 @@ class MemoryDNN:
         self,
         net,
         learning_rate = 0.01,
-        training_interval=10, 
-        batch_size=100, 
+        training_interval=10,
+        batch_size=100,
         memory_size=1000,
         output_graph=False
     ):
 
-        self.net = net  # the size of the DNN
+        self.net = net
         self.training_interval = training_interval      # learn every #training_interval
         self.lr = learning_rate
         self.batch_size = batch_size
         self.memory_size = memory_size
-        
+
         # store all binary actions
         self.enumerate_actions = []
 
@@ -50,13 +49,14 @@ class MemoryDNN:
         self._build_net()
 
     def _build_net(self):
-        self.model = keras.Sequential([
-                    layers.Dense(self.net[1], activation='relu'),  # the first hidden layer
-                    layers.Dense(self.net[2], activation='relu'),  # the second hidden layer
-                    layers.Dense(self.net[-1], activation='sigmoid')  # the output layer
-                ])
-
-        self.model.compile(optimizer=keras.optimizers.Adam(lr=self.lr), loss=tf.losses.binary_crossentropy, metrics=['accuracy'])
+        self.model = nn.Sequential(
+                nn.Linear(self.net[0], self.net[1]),
+                nn.ReLU(),
+                nn.Linear(self.net[1], self.net[2]),
+                nn.ReLU(),
+                nn.Linear(self.net[2], self.net[3]),
+                nn.Sigmoid()
+        )
 
     def remember(self, h, m):
         # replace the old memory with new memory
@@ -80,24 +80,32 @@ class MemoryDNN:
         else:
             sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
         batch_memory = self.memory[sample_index, :]
-        
-        h_train = batch_memory[:, 0: self.net[0]]
-        m_train = batch_memory[:, self.net[0]:]
-        
-        # print(h_train)          # (128, 10)
-        # print(m_train)          # (128, 10)
+
+        h_train = torch.Tensor(batch_memory[:, 0: self.net[0]])
+        m_train = torch.Tensor(batch_memory[:, self.net[0]:])
+
 
         # train the DNN
-        hist = self.model.fit(h_train, m_train, verbose=0)
-        self.cost = hist.history['loss'][0]
+        optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        criterion = nn.BCELoss()
+        self.model.train()
+        optimizer.zero_grad()
+        predict = self.model(h_train)
+        loss = criterion(predict, m_train)
+        loss.backward()
+        optimizer.step()
+
+        self.cost = loss.item()
         assert(self.cost > 0)
         self.cost_his.append(self.cost)
 
     def decode(self, h, k = 1, mode = 'OP'):
-        # to have batch dimension when feed into tf placeholder
-        h = h[np.newaxis, :]
+        # to have batch dimension when feed into Tensor
+        h = torch.Tensor(h[np.newaxis, :])
 
-        m_pred = self.model.predict(h)
+        self.model.eval()
+        m_pred = self.model(h)
+        m_pred = m_pred.detach().numpy()
 
         if mode is 'OP':
             return self.knm(m_pred[0], k)
@@ -105,13 +113,13 @@ class MemoryDNN:
             return self.knn(m_pred[0], k)
         else:
             print("The action selection must be 'OP' or 'KNN'")
-    
+
     def knm(self, m, k = 1):
-        # return k-nearest-mode
+        # return k order-preserving binary actions
         m_list = []
         # generate the ﬁrst binary ofﬂoading decision with respect to equation (8)
         m_list.append(1*(m>0.5))
-        
+
         if k > 1:
             # generate the remaining K-1 binary ofﬂoading decisions with respect to equation (9)
             m_abs = abs(m)
@@ -125,7 +133,7 @@ class MemoryDNN:
                     m_list.append(1*(m - m[idx_list[i]] >= 0))
 
         return m_list
-    
+
     def knn(self, m, k = 1):
         # list all 2^N binary offloading actions
         if len(self.enumerate_actions) is 0:
@@ -136,7 +144,7 @@ class MemoryDNN:
         sqd = ((self.enumerate_actions - m)**2).sum(1)
         idx = np.argsort(sqd)
         return self.enumerate_actions[idx[:k]]
-        
+
 
     def plot_cost(self):
         import matplotlib.pyplot as plt
@@ -144,3 +152,4 @@ class MemoryDNN:
         plt.ylabel('Training Loss')
         plt.xlabel('Time Frames')
         plt.show()
+
